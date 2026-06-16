db3abafa8097ecaf74d39009727c6d20a6178a55
Sisart003 <chrishart.estrada@gmail.com>
2026-06-17 05:40:37 +0800

    ChirpController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+Route::get('/', [ChirpController::class, 'index']);
